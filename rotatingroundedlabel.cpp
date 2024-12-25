a0e21d208ef69370c90fe573f0321912e0d362e8
droplazy <mx401@126.com>
2024-12-25 09:07:19 +0800

    图片现在可以正确旋转了

---
+++ rotatingroundedlabel.cpp
@@ -0,0 +1,6 @@
+#include "rotatingroundedlabel.h"
+
+RotatingRoundedLabel::RotatingRoundedLabel()
+{
+
+}
---
+++ rotatingroundedlabel.h
@@ -0,0 +1,12 @@
+#ifndef ROTATINGROUNDEDLABEL_H
+#define ROTATINGROUNDEDLABEL_H
+
+#include <QObject>
+
+class RotatingRoundedLabel
+{
+public:
+    RotatingRoundedLabel();
+};
+
+#endif // ROTATINGROUNDEDLABEL_H
