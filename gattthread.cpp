b926e0d25b204d28ea23d0045bf1b69f4020b8a6
droplazy <mx401@126.com>
2025-06-22 18:50:14 +0800

    gatt update

---
+++ gattthread.cpp
@@ -0,0 +1,6 @@
+#include "gattthread.h"
+
+gattthread::gattthread()
+{
+
+}
---
+++ gattthread.h
@@ -0,0 +1,12 @@
+#ifndef GATTTHREAD_H
+#define GATTTHREAD_H
+
+#include <QObject>
+
+class gattthread
+{
+public:
+    gattthread();
+};
+
+#endif // GATTTHREAD_H
